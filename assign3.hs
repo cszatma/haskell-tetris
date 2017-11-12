3be72b6d55409f44f994b8738023b5d78b500dd0
cszatma <christopherszatmary@gmail.com>
2017-11-12 12:06:17 -0500

    Added functions for processing input and getting game commands

--- assign3.hs
+++ assign3.hs
@@ -1,7 +1,31 @@
+module Main where
+
+-- Import necessary libraries
 import System.IO
 import Data.List
 import Data.String
+import Board
 
+-- Main function
+main :: IO ()
+main = do
+    input <- getContents
+    putStr $ show $ readFrom input
+
+readFrom input = do
+    --let inputLines = lines input
+    --let commands = map (words) inputLines
+    --createBoard $ (getCommands input) !! 0
+    getCommands input
+
+-- Returns a list of commands
+-- Each command is a list of strings that make up the command
+getCommands inputString = do
+    map (words) $ lines inputString
+
+runCommands commands = do
+    createBoard $ commands !! 0
+{-
 words :: String -> [String]
 words []  = []
 words xxs@(x:xs)
@@ -10,6 +34,8 @@ words xxs@(x:xs)
                   where (ys, rest) = break (== ' ') xxs
 
 
-  formatRow :: Car -> String
+formatRow :: Car -> String
   formatRow (a, d:ds, c, x:xs) = a ++ " | " ++ x ++ concat xs ++ " | " ++ show c ++ "
 -- cycle list helps u  repeat a list infinitely cycle[list]
+-}
+
