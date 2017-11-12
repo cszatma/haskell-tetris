e12e812f89354bb3358f5c9e2c979435bd9cae08
faizaabedin <33582390+faizaabedin@users.noreply.github.com>
2017-11-11 22:10:14 -0500

    Add files via upload

--- assign3.hs
+++ assign3.hs
@@ -1,4 +1,15 @@
-module Main where
+import System.IO
+import Data.List
+import Data.String
 
-main :: IO ()
-main = putStrLn "Hello, World"
+words :: String -> [String]
+words []  = []
+words xxs@(x:xs)
+  | x == ' '  = words xs
+  | otherwise = ys : words rest
+                  where (ys, rest) = break (== ' ') xxs
+
+
+  formatRow :: Car -> String
+  formatRow (a, d:ds, c, x:xs) = a ++ " | " ++ x ++ concat xs ++ " | " ++ show c ++ "
+-- cycle list helps u  repeat a list infinitely cycle[list]
