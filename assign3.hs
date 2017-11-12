fe51d9a327cb6304ef0b36afe3f2a75040ee43e1
Christopher Szatmary <christopherszatmary@gmail.com>
2017-11-12 15:15:21 -0500

    Added functions to create and print the board. Removed unecessary code from assign3.hs

--- assign3.hs
+++ assign3.hs
@@ -25,17 +25,4 @@ getCommands inputString = do
 
 runCommands commands = do
     createBoard $ commands !! 0
-{-
-words :: String -> [String]
-words []  = []
-words xxs@(x:xs)
-  | x == ' '  = words xs
-  | otherwise = ys : words rest
-                  where (ys, rest) = break (== ' ') xxs
-
-
-formatRow :: Car -> String
-  formatRow (a, d:ds, c, x:xs) = a ++ " | " ++ x ++ concat xs ++ " | " ++ show c ++ "
--- cycle list helps u  repeat a list infinitely cycle[list]
--}
 
--- board.hs
+++ board.hs
@@ -1,4 +1,10 @@
 module Board where
 
 createBoard list = do
-    map (read :: String -> Int) $ tail list
+    let [width, height] = map (read :: String -> Int) $ tail list
+    take height $ repeat $ take width $ repeat " "     
+
+printBoard board = do
+    mapM_ putStrLn $ map (\row -> "|" ++ (concat row) ++ "|") board
+    let rowLength = length $ board !! 0
+    putStrLn $ "+" ++ (take rowLength $ repeat '-') ++ "+"
