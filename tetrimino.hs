dbe1aef8622d489d7bfa0dff7f6fc899090108f9
Christopher Szatmary <christopherszatmary@gmail.com>
2017-11-12 16:34:34 -0500

    Created Tetrimino module, added functions for constructing tetriminos

---
+++ tetrimino.hs
@@ -0,0 +1,34 @@
+module Tetrimino where
+
+-- Define Tetrimino type
+data Tetrimino = Tetrimino [[Int]] (Int, Int) String
+    deriving Show
+
+-- Functions for getting properties
+points :: Tetrimino -> [[Int]]
+points (Tetrimino pointsList _ _) = pointsList
+
+position :: Tetrimino -> (Int, Int)
+position (Tetrimino _ pos _) = pos
+
+typeString :: Tetrimino -> String
+typeString (Tetrimino _ _ str) = str
+
+-- Creating Tetriminos
+createType :: Int -> (Int, Int) -> Tetrimino
+createType 1 pos = Tetrimino [[0, 0], [1, 0], [0, -1], [1, -1]] pos "y"
+
+createType 2 pos = Tetrimino [[-1, -1], [0, -1], [0, 0], [1, 0]] pos "r"
+
+createType 3 pos = Tetrimino [[-1, 0], [0, 0], [0, -1], [1, -1]] pos "g"
+
+createType 4 pos = Tetrimino [[-1, -1], [-1, 0], [0, 0], [1, 0]] pos "b"
+
+createType 5 pos = Tetrimino [[-1, 0], [0, 0], [1, 0], [1, -1]] pos "o"
+
+createType 6 pos = Tetrimino [[-1, 0], [0, 0], [1, 0], [0, -1]] pos "p"
+
+createType 7 pos = Tetrimino [[-1, 0], [0, 0], [1, 0], [2, 0]] pos "c"
+
+getPosOnBoard :: Tetrimino -> [[Int]]
+getPosOnBoard (Tetrimino points pos _) = map (\point -> [point !! 0 + fst pos, point !! 1 + snd pos]) points
