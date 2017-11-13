d707ae2e7cc7a6142655c23c6381d8e02e850a5b
faizaabedin <33582390+faizaabedin@users.noreply.github.com>
2017-11-12 20:13:55 -0500

    Update movesArray.hs

--- movesArray.hs
+++ movesArray.hs
@@ -1,6 +1,5 @@
-import Data.List
-import Data.String
+
 -- just kept as a funtion not a module
 -- it "moves l l+ .." -> "ll+.."
-moveArray :: [String] -> String
-moveArray a =filter (/=' ') (concat $ tail a)
+moveArray list = do
+filter (/=' ') (concat $ tail a)
